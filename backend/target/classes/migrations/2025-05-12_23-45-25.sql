f1af5a735c5c5a2affd376ff26aa6e01ae002b82
MariaBelenF <fontanamariabelen@gmail.com>
2025-05-26 19:50:58 -0300

    Ignorando correctamente carpeta backend/target y limpieza de archivos compilados

--- backend/target/classes/migrations/2025-05-12_23-45-25.sql
+++
@@ -1,9 +0,0 @@
--- liquibase formatted sql
-
--- changeset your.name:2
-CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
-    
-);
